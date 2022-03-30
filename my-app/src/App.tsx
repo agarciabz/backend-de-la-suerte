816fe9c0f6c92f0010b94b842001fe41d5a6ca55
agarciabz <agusgb93@gmail.com>
2022-03-30 22:02:32 +0200

    Add mining with directus

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,33 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-const url = `${process.env.REACT_APP_DIRECTUS_URL}/items/emoji/1`;
+const emojiAPI = `${process.env.REACT_APP_DIRECTUS_URL}/items/emoji`;
+const settingsAPI = `${process.env.REACT_APP_DIRECTUS_URL}/items/settings`;
 
 function EmojiDisplay(props: any) {
-  const {value} = props;
+  const {value, onClick, mining} = props;
   return (
-    <p>{value}</p>
+    <div>
+      <p>{value}</p>
+      {mining ? <p>Picando...</p> : <></>}
+      {value === '👑' ? <p>¡Enhorabuena!</p> : <></>}
+      <button onClick={onClick} >⛏️</button>
+    </div>
   );
 }
 
 function App() {
-  const [state, setState] = useState('🤷');
-
-  useEffect(() => {
-    axios.get(url).then((res) => {
-      setState(res.data.data.character);
-      console.log(res.data.data.character)
-    }).catch(error => {
-      setState('🤷');
-      console.log(error);
-    });
+  const [state, setState] = useState({
+    result: '🤷',
+    mining: false,
   });
+
+  const handlePick = () => {
+      setState({
+        ...state,
+        mining: true
+      });
+      updatePicks();
+  }
+
+  const updatePicks = async () => {
+    const count = +(await axios.get(`${settingsAPI}/1`)).data.data.value;
+    const current = +(await axios.patch(`${settingsAPI}/1`, { value: count + 1 })).data.data.value;
+    const goal = +(await axios.get(`${settingsAPI}/2`)).data.data.value;
+    if(current >= goal) {
+      const gold = (await axios.get(`${emojiAPI}/1`)).data.data.character;
+      setState({
+        result: gold,
+        mining: false
+      });
+    } else {
+      setState({
+        ...state,
+        mining: false,
+      })
+    }
+  }
+
+   useEffect(() => {
+    axios.patch(`${settingsAPI}/1`, { value: 0}).then();
+  }, []); 
+ 
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <EmojiDisplay value={state}></EmojiDisplay>
+        <EmojiDisplay value={state.result} mining={state.mining} onClick={() => handlePick()}></EmojiDisplay>
       </header>
     </div>
   );
